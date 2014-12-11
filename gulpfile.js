65758e6ee2bfc819e54fd16bf1ce82751d1e4506
Hugo Caillard <c.hugo.188@gmail.com>
2014-12-11 16:41:41 +0100

    Set up gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 var gulp       = require('gulp');
 var browserify = require('gulp-browserify');
 var livereload = require('gulp-livereload');
+var sass       = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('watch', function() {
-  gulp.watch(__dirname+'/src/js/**/*.js', ['js']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+
+// SCSS Files
+gulp.task('sass', function () {
+  gulp.src('./src/sass/main.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(livereload());
 });
 
 // JS Files - Development
@@ -21,4 +35,4 @@ gulp.task('js', function() {
 /**
   * DEFAULT TASK
 */
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'sass', 'watch']);
