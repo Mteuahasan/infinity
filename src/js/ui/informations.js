a7fd7a4a80107a3fb9208e421184c4bae1c3ffaa
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2014-12-15 18:04:01 +0100

    Tim update

--- src/js/ui/informations.js
+++ src/js/ui/informations.js
@@ -3,7 +3,7 @@ var $ = require('../tools');
 var informations = {
   init: function() {
     // usage of tools
-    // var content = $.getId('#id');
+    // var content = $.byId('#id');
 
     // Loop elements
     // var elements = $.selA('.btn');
@@ -11,6 +11,49 @@ var informations = {
     // $.loopEl(elements, function(el) {
     //   el.classList.add('hover');
     // });
+    
+    var startDate = new Date();
+    var endDate;
+    
+    
+    function theTime(){
+      var date = new Date();
+      endDate = date;
+      var hh = date.getHours();
+      var mm = date.getMinutes();
+      var ss = date.getSeconds();
+      return hh+':'+mm+':'+ss;
+    }
+    
+    function dateDiff(date1, date2){
+        var diff = {}                    
+        var tmp = date2 - date1;
+
+        tmp = Math.floor(tmp/1000);             
+        diff.sec = tmp % 60;
+
+        tmp = Math.floor((tmp-diff.sec)/60);    
+        diff.min = tmp % 60;                    
+
+        tmp = Math.floor((tmp-diff.min)/60); 
+        diff.hour = tmp % 24;                   
+
+        tmp = Math.floor((tmp-diff.hour)/24); 
+        diff.day = tmp;
+
+        return diff;
+    }
+    
+    var currentTime = $.byId('current_time');
+    var timeLaps = $.byId('time');
+    
+    setInterval(function(){
+      currentTime.innerHTML=theTime();
+      var delta = dateDiff(startDate,endDate);
+      timeLaps.innerHTML=delta.hour+':'+delta.min+':'+delta.sec;
+      //timeLaps.innerHTML=timeLaps(timeLaps.innerHTML);
+    },1000);
+
 
 
   }
