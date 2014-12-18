802875587f3947e1e338570d3d4e16a0ee41a684
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2014-12-18 16:51:12 +0100

    State transitions

--- src/js/ui/interactions.js
+++ src/js/ui/interactions.js
@@ -9,6 +9,7 @@ var interactions = {
   init:function() {
     interactions.masseInteraction();
     interactions.fusionInteraction();
+    interactions.demarrageSyst();
   },
 
   masseInteraction:function() {
@@ -33,7 +34,47 @@ var interactions = {
     for (var i=0; i < elements.length; i++) {
       // Apply fusion rate
     }
+  },
+  demarrageSyst:function(){
+    var button = $.byId('button-syst');
+    var etapes = document.querySelectorAll('.etape');
+    var forms = document.querySelectorAll('#popup-box form');
+    var popupBox = $.byId('popup-box');
+    
+    button.addEventListener('click',function(e){ // Click on REDEMARRAGE
+      e.preventDefault();
+      button.innerHTML="<a href=\"\">[ REDEMARRAGE SYST. ]</a>";
+      button.removeEventListener('click');
+      popupBox.classList.add('active');
+      etapes[0].style.cssText="top:0px;";
+    },false);
+    
+    forms[0].addEventListener('submit',function(e){
+      e.preventDefault();
+      // SET ETAPE 1 PARAM
+      etapes[0].style.cssText="top:101%;";
+      setTimeout(function(){etapes[1].style.cssText="top:0;";},1000);
+      return;
+    },false);
+    
+    forms[1].addEventListener('submit',function(e){
+      e.preventDefault();
+      // SET ETAPE 3 PARAM
+      etapes[1].style.cssText="top:101%;";
+      setTimeout(function(){etapes[2].style.cssText="top:0;"},1000);
+      return;
+    },false);
+    
+    forms[2].addEventListener('submit',function(e){
+      e.preventDefault();
+        // SET ETAPE 3 PARAM
+        etapes[2].style.cssText="top:101%;";
+        popupBox.classList.remove('active');
+      return;
+    },false);
+    
   }
+  
 };
 
 module.exports = interactions;
